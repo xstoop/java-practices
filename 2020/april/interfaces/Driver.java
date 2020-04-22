b001cad72f1562451fee2cf1621d01d461d963e2
xstoop <xstoop@163.com>
2020-04-22 15:48:41 +0800

    interface

---
+++ 2020/april/interfaces/Driver.java
@@ -0,0 +1,5 @@
+package april.interfaces;
+
+interface Driver {
+    void drive();
+}
---
+++ 2020/april/interfaces/Hello.java
@@ -0,0 +1,8 @@
+package april.interfaces;
+
+/**
+ * 接口可以继承接口
+ */
+interface Hello extends Person {
+    void say();
+}
