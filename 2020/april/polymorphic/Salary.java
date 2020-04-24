581c150faad485dde69006023f1682ba629f6169
xstoop <xstoop@163.com>
2020-04-24 18:23:38 +0800

    多态

---
+++ 2020/april/polymorphic/Salary.java
@@ -0,0 +1,17 @@
+package april.polymorphic;
+
+public class Salary extends Income {
+
+    public Salary(double income) {
+        super(income);
+    }
+
+    @Override
+    public double getTax() {
+        // 工资交税去掉5000基数
+        if (income <= 5000) {
+            return 0;
+        }
+        return (income - 5000) * 0.2;
+    }
+}
