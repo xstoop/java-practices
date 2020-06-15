b9a4a9be3e68168f6ca2377d2f31ddceddf99eeb
xstoop <xstoop@163.com>
2020-06-15 09:59:15 +0800

    sprint boot mysql and druid

--- spring-boot-book-v2/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-boot-book-v2/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
+import com.example.demo.model.AyUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void mysqlTest() {
+        String sql = "select id,name,password from ay_user";
+        List<AyUser> userList =
+                (List<AyUser>) jdbcTemplate.query(sql, new RowMapper<AyUser>() {
+                    @Override
+                    public AyUser mapRow(ResultSet resultSet, int i) throws SQLException {
+                        AyUser user = new AyUser();
+                        user.setId(resultSet.getInt("id"));
+                        user.setName(resultSet.getString("name"));
+                        user.setPassword(resultSet.getString("password"));
+
+                        return user;
+                    }
+                });
+        System.out.println("查询成功");
+        for (AyUser user : userList) {
+            System.out.println("[id]:" + user.getId() + ";[name]:" + user.getName() + ";[password]:" + user.getPassword());
+        }
+    }
 }
